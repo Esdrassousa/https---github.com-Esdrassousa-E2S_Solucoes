ae2e019aa0bcb1153203e73da5a4e109f89c5b88
esdrassousa <esdrassousasoares@hotmail.com>
2023-05-18 10:10:41 -0300

    colocando jsx

--- y/teste.jsx
+++ y/teste.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    (<h1>E2S SALVE</h1>)
+
+
   </React.StrictMode>,
 )
