4ada8de1fc2d1f36f730b6d0f56b9608166bb978
esdrassousa <esdrassousasoares@hotmail.com>
2023-05-18 15:49:07 -0300

    somente html

---
+++ y/teste.js
@@ -0,0 +1,12 @@
+
+
+
+var botao = document.getElementById("meuBotao");
+botao.addEventListener("click", addText);
+
+function addText(){
+
+  //var text = document.getElementById("button").value
+  console.log("textobotao")
+
+}
